1370649943daa9c794434d4319da9b48433accd9
Rémi Hay Rimbault <remihayrimbault@eduroam-n2ng-150031.local.univ-savoie.fr>
2020-10-09 15:14:47 +0200

    Modification du fichier single

--- single.php
+++ single.php
@@ -1,8 +1,14 @@
-
-    <?php 
-    
-    get_header();
-
-    get_footer(); 
-    
-    ?>
+<?php get_header();?>
+<section>
+  <!--The Loop (la boucle)-->
+  <?php if( have_posts() ) :
+      while ( have_posts() ) :
+          the_post(); ?>
+  <article>
+      <h2><?php the_title(); ?></h2>
+      <div><?php the_content(); ?></div>
+  </article>
+  <?php endwhile;else:?><p> Désolé pas d'article!</p>
+  <?php endif; ?>
+<section>
+<?php get_footer();?>
