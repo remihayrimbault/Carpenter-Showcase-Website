db741aa54c5f1177ea4b44695003233960b5c143
Rémi Hay Rimbault <remihayrimbault@eduroam-n2ng-150031.local.univ-savoie.fr>
2020-10-09 15:59:21 +0200

    Modification du fichier functions pour prendre en compte le css et js

--- functions.php
+++ functions.php
@@ -1,11 +1,18 @@
 <?php
 
-// ajout du title
+  // ajout du title
 
-function ajout_titre() {
-    add_theme_support("title-tag");
-};
+  function ajout_titre() {
+      add_theme_support("title-tag");
+  };
 
-add_action('after_setup_theme', 'ajout_titre');
+  function ajout_style(){
+    wp_enqueue_style('general',get_template_directory_uri().'/style.css');
+    wp_enqueue_script('special',get_template_directory_uri().'js/script.js');
+  };
+
+  add_action('after_setup_theme', 'ajout_titre');
+
+  add_action('wp_enqueue_scripts','ajout_style');
 
 ?>
