bfa16ebded6bdf9e0427927215de64e3406611c7
Rémi Hay Rimbault <remihayrimbault@eduroam-n2ng-150031.local.univ-savoie.fr>
2020-10-09 15:20:41 +0200

    Modification permalink dans le index

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
-
-    <?php 
-    
-    get_header();
-
-    get_footer(); 
-    
-    ?>
+<?php get_header();?>
+<a href="<?php the_permalink();?>"><?php the_title(); ?> </a>
+<section>
+  <!--The Loop (la boucle)-->
+  <?php if( have_posts() ) :
+      while ( have_posts() ) :
+          the_post(); ?>
+  <article>
+      <h2><?php the_title(); ?></h2>
+      <div><?php the_content(); ?></div>
+  </article>
+  <?php endwhile;else:?><p> Désolé pas d'article!</p>
+  <?php endif; ?>
+<section>
+<?php get_footer();?>
