f3456a87b318cc6f6176a01cc8ac6f7cb857e2dc
remihayrimbault <remi.hayrimbault@gmail.com>
2020-11-19 13:05:47 +0100

    Mise en place du menu pour les ecrans tablette et smartphone

--- anim.js
+++ anim.js
@@ -1,15 +1,15 @@
 
 function active_carte($nom) {
     var $idStr = $nom.id;
-    $('#pergola').removeClass('active');
-    $('#extension').removeClass('active');
-    $('#cabane').removeClass('active');
-    $('#mezzanine').removeClass('active');
-    $('#plus').removeClass('active');
-    $('#charpente').removeClass('active');
+    $('#pergola').removeClass('actif');
+    $('#extension').removeClass('actif');
+    $('#cabane').removeClass('actif');
+    $('#mezzanine').removeClass('actif');
+    $('#plus').removeClass('actif');
+    $('#charpente').removeClass('actif');
     $('#sauna').removeClass('active');
-    $('#abris').removeClass('active');
-    $('#terrasse').removeClass('active');
+    $('#abris').removeClass('actif');
+    $('#terrasse').removeClass('actif');
     $('.carousel_terrasse').css('display', 'none');
     $('.carousel_terrasse').slick('slickPause');
 		$('.carousel_pergola').css('display', 'none');
@@ -28,40 +28,29 @@ function active_carte($nom) {
     $('.carousel_sauna').slick('slickPause');
 		$('.carousel_abris').css('display', 'none');
     $('.carousel_abris').slick('slickPause');
-    $($nom).addClass('active');
+    $($nom).addClass('actif');
     console.log($idStr);
     $(".carousel_"+$idStr).css('display', 'block');
     $(".carousel_"+$idStr).slick('refresh');
 };
 
-// JavaScript Document
-
- // Toggle Icone hamburger
-    $(document).ready(function(){
-        $('.barres').click(function(){
-			$('.barres').toggleClass('active');
-			$('.menu_responsive').css('height', '100%');
-			document.getElementById(".barres");
-        })
-    })
-
-    // JavaScript Document
-
      // Toggle Icone hamburger
-        $(document).ready(function(){
             $('.barres').click(function(){
-                $('.barres').toggleClass('active');
-    			document.getElementById(".barres");
-            })
-        })
+                $(this).toggleClass('active');
+            });
 
     	var $hamburger = $('.barres');
 
     	var hamburgerMotion = new TimelineMax()
     	.to(".menu_responsive", 1.2, {left: '0%',ease: Expo.easeInOut,})
-    	.staggerFrom(".menu_responsive ul li", 0.5, {x: -100, opacity: 0}, 0.1)
+    	.staggerFrom(".menu_responsive > ul > li", 0.5, {x: -100, opacity: 0}, 0.1)
     	.reverse()
 
     	$hamburger.on('click', function(e) {
       		hamburgerMotion.reversed(!hamburgerMotion.reversed());
     	});
+
+      $('.menu_responsive a').on('click', function(){
+    		hamburgerMotion.reversed(!hamburgerMotion.reversed());
+    		$('.barres').toggleClass('active');
+    	})
